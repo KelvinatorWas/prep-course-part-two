b737a98a4fc86ecab840e6b7b88d092f485f9535
KelvinatorWas <liepkalnskalvis@gmail.com>
2023-10-07 00:00:36 +0300

    02 <- Done

--- src/02-capitalize/index.ts
+++ src/02-capitalize/index.ts
@@ -11,6 +11,8 @@
  * capitalize('look, it is working!') === 'Look, It Is Working!'
  */
 
-function capitalize(str: string) {}
+function capitalize(str: string) {
+    return str.split(" ").map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
+}
 
 export { capitalize };
