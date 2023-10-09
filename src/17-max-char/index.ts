9c3162f3a62ea719daf7a9dd1edde20353be8882
KelvinatorWas <liepkalnskalvis@gmail.com>
2023-10-09 18:56:16 +0300

    17 <- Done

--- src/17-max-char/index.ts
+++ src/17-max-char/index.ts
@@ -9,6 +9,18 @@
  * maxChar("apple 1231111") === "1"
  */
 
-function maxChar(str: string) {}
+function maxChar(str: string): string {
+    const charCount: Record<string, number> = {};
+    str.toLowerCase().replace(/\s+/g, "").split("").forEach(char => {charCount[char] = (charCount[char] || 0) + 1;});
+    let maxChar = "";
+    let maxCount = 0;
+    for (const char in charCount) {
+        if (charCount[char] > maxCount) {
+            maxChar = char;
+            maxCount = charCount[char];
+        }
+    }
+    return maxChar;
+}
 
 export { maxChar };
