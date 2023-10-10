1ef728b71761d8b57043fb3c8ab5be9c73a55e48
KelvinatorWas <liepkalnskalvis@gmail.com>
2023-10-10 23:19:19 +0300

    00 <- removed uneeded bits

--- src/00-acronym/index.ts
+++ src/00-acronym/index.ts
@@ -1,14 +1,14 @@
 /**
  * Convert a phrase to its acronym.
  *
- * Techies love their TLA (Three Letter Acronyms)!
+ * Techies love their TLA ("Three" "Letter" "Acronyms")!
  *
  * Help generate some jargon by writing a program that converts a long name like Portable Network Graphics to its acronym (PNG).
  */
 
 function parse(input: string) {
-    const str:string[] = input.toUpperCase().replace(/[^a-zA-Z0-9']/g, " ").split(" ");
-    return str.map((word:string) => word.charAt(0)).join(""); 
+    const str:string[] = input.toUpperCase().replace(/[^A-Z0-9']/g, " ").split(" ");
+    return str.map((word:string) => word[0]).join(""); 
 }
 
 export { parse };
